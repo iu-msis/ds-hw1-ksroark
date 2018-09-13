2e97e5f45251cae91a54450f6d055c2e97c4bb4b
Kyle <ksroark@iu.edu>
2018-09-13 08:43:10 -0400

    add fetch method

--- public/js/user.app.js
+++ public/js/user.app.js
@@ -10,76 +10,77 @@ var userApp = new Vue({
     "projected_spend": "4740500",
     "weekly_effort_target": 400,
     results: [
-    {
-      "gender": "male",
-      "name": {
-        "title": "mr",
-        "first": "rolf",
-        "last": "hegdal"
-      },
-      "location": {
-        "street": "ljan terrasse 346",
-        "city": "vear",
-        "state": "rogaland",
-        "postcode": "3095",
-        "coordinates": {
-          "latitude": "54.8646",
-          "longitude": "-97.3136"
+      {
+        gender: '',
+        name: {
+          title: '',
+          first: '',
+          last: ''
         },
-        "timezone": {
-          "offset": "-10:00",
-          "description": "Hawaii"
-        }
-      },
-      "email": "rolf.hegdal@example.com",
-      "login": {
-        "uuid": "c4168eac-84b8-46ea-b735-c9da9bfb97fd",
-        "username": "bluefrog786",
-        "password": "ingrid",
-        "salt": "GtRFz4NE",
-        "md5": "5c581c5748fc8c35bd7f16eac9efbb55",
-        "sha1": "c3feb8887abed9ec1561b9aa2c9f58de21d1d3d9",
-        "sha256": "684c478a98b43f1ef1703b35b8bbf61b27dbc93d52acd515e141e97e04447712"
-      },
-      "dob": {
-        "date": "1975-11-12T06:34:44Z",
-        "age": 42
-      },
-      "registered": {
-        "date": "2015-11-04T22:09:36Z",
-        "age": 2
-      },
-      "phone": "66976498",
-      "cell": "40652479",
-      "id": {
-        "name": "FN",
-        "value": "12117533881"
-      },
-      "picture": {
-        "large": "https://randomuser.me/api/portraits/men/65.jpg",
-        "medium": "https://randomuser.me/api/portraits/med/men/65.jpg",
-        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/65.jpg"
-      },
-      "nat": "NO"
-    }
-  ]
+        location: {
+          street: '',
+          city: '',
+          state: '',
+          postcode: 0,
+          coordinates: {
+            latitude: '',
+            longitude: ''
+          },
+          timezone: {
+            offset: '',
+            description: ''
+          }
+        },
+        email: '',
+        login: {
+          uuid: '',
+          username: '',
+          password: '',
+          salt: '',
+          md5: '',
+          sha1: '',
+          sha256: ''
+        },
+        dob: {
+          date: '',
+          age: 0
+        },
+        registered: {
+          date: '',
+          age: 0
+        },
+        phone: '',
+        cell: '',
+        id: {
+          name: '',
+          value: ''
+        },
+        picture: {
+          large: '',
+          medium: '',
+          thumbnail: ''
+        },
+        nat: ''
+      }
+    ]
   },
   computed: {
     result_age: function () {
       return moment(this.dob.date).diff(moment(), 'days');
     }
   },
-
-/*  methods: {
-    fetchTasks () {
-      fetch( 'https://randomuser.me/' )
-      .then( function (response) {return response.json()} )
-      .then( function (json) {dashboardApp.tasks = json})
-      .catch( function(err) {
+  methods: {
+    fetchResults () {
+      fetch ( 'https://randomuser.me/api/' )
+      .then( response => response.json() )
+      .then( json => {userApp.results = json } )
+      .catch ( function(err) {
         console.log('TASK FETCH ERROR:');
         console.log(err);
       })
-    }
-  } */
-
+      }
+    },
+  created () {
+    this.fetchResults()
+  }
 });
